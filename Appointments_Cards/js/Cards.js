f5649326bee7aeb916efbdc30f63304717d41800
Serhii <serhii.savrak@gmail.com>
2022-01-02 23:30:04 +0200

    resolved edit btn issue

--- Appointments_Cards/js/Cards.js
+++ Appointments_Cards/js/Cards.js
@@ -99,128 +99,47 @@ class Cards {
       "test"
     );
 
-    e.target.closest(".card-wrapper").remove();
     const newModal = modalEditWindow.render();
     modalEditWindow.openModal();
     constans.ROOT.append(newModal);
-    const modal1 = document.getElementById("modalTitle");
+    const modal = document.getElementById("modalTitle");
 
-    // const visit = new Visit(modal1, "");
-    // visit.render();
+    const editForm = new Visit(modal, "");
 
-    // const form = document.getElementById("visit-form");
-    // form.id = "test";
-    const form = document.createElement("form");
-    form.addEventListener("submit", this.submitHandler);
-    form.id = "edit-form";
+    editForm.submitHandler = (e) => {
+      e.preventDefault();
 
-    const purposeLabel = document.createElement("label");
-    purposeLabel.textContent = "Purpose of visit:";
-    // purposeLabel.setAttribute("for", "purpose");
+      const inputs = [...document.getElementsByClassName("card-input")];
+      const [purpose, description, patientName] = inputs;
 
-    const purposeOfVisit = new Input({
-      type: "text",
-      name: "purpose",
-      isRequired: true,
-      id: "purpose",
-      classes: ["purpose-of-visit", "card-input"],
-      placeholder: "start typing...",
-      errorText: "all fields must be filled!",
-    }).render();
-    purposeLabel.append(purposeOfVisit);
+      const data = {
+        title: purpose.value,
+        description: description.value,
+        patientName: patientName.value,
+      };
 
-    const descriptionLabel = document.createElement("label");
-    descriptionLabel.textContent = "Short description of visit:";
-    // descriptionLabel.setAttribute("for", "description");
-
-    const visitDescription = new Input({
-      type: "text",
-      name: "description",
-      isRequired: true,
-      id: "description",
-      classes: ["visit-description", "card-input"],
-      placeholder: "start typing...",
-      errorText: "all fields must be filled!",
-    }).render();
-    descriptionLabel.append(visitDescription);
-
-    const patientNameLabel = document.createElement("label");
-    patientNameLabel.textContent = "Full name of patient:";
-    // patientNameLabel.setAttribute("for", "patientName");
-
-    const patientName = new Input({
-      type: "text",
-      name: "patientName",
-      isRequired: true,
-      id: "patientName",
-      classes: ["patient-name", "card-input"],
-      placeholder: "start typing...",
-      errorText: "all fields must be filled!",
-    }).render();
-    patientNameLabel.append(patientName);
-
-    const dateOfVisitLabel = document.createElement("label");
-    dateOfVisitLabel.textContent = "Full name of patient:";
-    // dateOfVisitLabel.setAttribute("for", "visitDate");
-
-    const dateOfVisit = new Input({
-      type: "date",
-      name: "visitDate",
-      isRequired: true,
-      id: "visitDate",
-      classes: ["visit-date", "card-input"],
-      placeholder: "start typing...",
-      errorText: "all fields must be filled!",
-    }).render();
-    dateOfVisitLabel.append(dateOfVisit);
-
-    const btn = document.createElement("button");
-    btn.textContent = "Button";
-    btn.id = "submit-btn";
-    btn.classList.add("btn-success", "btn", "rounded");
-
-    form.append(
-      purposeLabel,
-      descriptionLabel,
-      patientNameLabel,
-      dateOfVisitLabel,
-      btn
-    );
-
-    form.style.display = "flex";
-    // const [purpose, description, patientName, visitDate] = form;
-
-    const btn2 = form.lastChild;
-    btn.id = "edit-btn";
-    btn.textContent = "Edit";
-    purpose.value = "type new value";
-    description.value = "type new value";
-    patientName.value = "type new value";
-
-    modalEditWindow.append(form);
-
-    this.cardWrapper.remove();
-  };
-
-  submitHandler(e) {
-    e.preventDefault();
-
-    const data = {
-      title: purpose.value,
-      description: description.value,
-      patientName: patientName.value,
+      const request = new Requests(constans.URL);
+      request
+        .put("", JSON.stringify(data), this.id, constans.token)
+        .then((resp) => resp.json())
+        .then((data) => {
+          const card = new Cards(data, constans.fieldCardsContainer);
+          card.render();
+          this.cardWrapper.remove();
+          document.getElementById("edit-modal").remove();
+          console.log("PUT is succesful");
+        });
     };
-    // newModal.remove();
-    // form.remove();
-    const request = new Requests(constans.URL);
-    request
-      .put("", JSON.stringify(data), this.id, constans.token)
-      .then((resp) => resp.json())
-      .then((data) => {
-        // const card = new Cards(data, constans.fieldCardsContainer);
-        // card.cardRender();
-      });
-  }
+
+    editForm.render();
+
+    const inputs = [...document.getElementsByClassName("card-input")];
+    const [purpose, description, patientName] = inputs;
+
+    purpose.value = this.title;
+    description.value = this.description;
+    patientName.value = this.patientName;
+  };
 }
 
 export default Cards;
